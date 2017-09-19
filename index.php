4dff65edbe17ae9b4f647852ba57e9ca3105c7b7
Kazunori Oyama <kazunori0326.t@gmail.com>
2017-09-19 21:56:38 +0900

    Bootstrap利用をやめた

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <?php get_header();?>
 <main class="<?php post_class();?>">
- <?php if(have_posts):
- while(have_posts()):the_post();?>
-  <div class="container-fluid ">
-			<div class="row main_image">
-				<div class="top_message_box">
-				<h2 class="message">
-					<?php the_title();?>
-				</h2>
-				</div>
-				<img src="https://placehold.jp/1280x330.png" alt="">
-			</div>
-		</div>
-  <div class="container" id="main_section">
-			<div class="row">
+ <?php if(have_posts):?>
+ 		<?php while(have_posts()):the_post();?>
 				<?php the_content();?>
-			</div>
-		</div>
 		<?php endwhile;?>
 <?php endif;?>
 </main>
