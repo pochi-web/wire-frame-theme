18cfdbd111c66899c352b403763576599ce586b9
Kazunori Oyama <kazunori0326.t@gmail.com>
2017-09-23 22:05:17 +0900

    push some modified

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
-<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'my_extra_menu_class' ) );?>
+<?php wp_nav_menu( array( 
+	'theme_location' => 'footer-menu', 
+	'container_class' => 'my_extra_menu_class' ) );
+?>
 <?php wp_footer();?>
 	
 </body>
