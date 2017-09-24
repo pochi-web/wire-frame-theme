caee3b25ca32d9d4ddefdcecbdae4b9c8e6b7023
Kazunori Oyama <kazunori0326.t@gmail.com>
2017-09-24 22:16:44 +0900

    add some modified

--- functions.php
+++ functions.php
@@ -1,8 +1,17 @@
 <?php
 //load css file
 function theme_style(){
+wp_enqueue_style('base',get_template_directory_uri().'/css/base.css',
+				 array(
+	'element',
+	'modifier'
+));
+wp_enqueue_style('element',get_template_directory_uri().'/css/base.css');
+wp_enqueue_style('modifier',get_template_directory_uri().'/css/base.css');
 wp_enqueue_style('style',get_stylesheet_uri(),'');
-}
+};
+add_action( 'wp_enqueue_scripts', 'theme_style' );
+
 //define custom menus
 function register_my_menus() {
   register_nav_menus(
@@ -13,8 +22,5 @@ function register_my_menus() {
   );
 }
 add_action( 'init', 'register_my_menus' );
-
-//end of custom menus
-
 
 ?>
